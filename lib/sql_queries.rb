af30078160fa79afc1e4ed177a1c641a2d196391
valeria campilongo <cvaleriac@hotmail.it>
2018-08-17 18:20:32 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -4,7 +4,7 @@ end
  def selects_all_bears_names_and_orders_in_alphabetical_order
   "SELECT name FROM bears ORDER BY name"
 end
- def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest	
+ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
    "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age"
 end
  def selects_oldest_bear_and_returns_name_and_age
@@ -12,7 +12,7 @@ end
 end
  def select_youngest_bear_and_returns_name_and_age
   "SELECT name, age FROM bears WHERE age = (SELECT MIN(age) FROM bears)"
-end	end
+end
  def selects_most_prominent_color_and_returns_with_count
    "SELECT color, COUNT(color) FROM bears WHERE color = 'dark brown'"
 end
