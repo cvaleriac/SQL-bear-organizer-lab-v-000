5b2e81ee49c5a85ac8d76307ece09c192a710e1b
valeria campilongo <cvaleriac@hotmail.it>
2018-08-17 18:20:52 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,7 +16,7 @@ end
  def selects_most_prominent_color_and_returns_with_count
    "SELECT color, COUNT(color) FROM bears WHERE color = 'dark brown'"
 end
- def counts_number_of_bears_with_goofy_temperaments	
+ def counts_number_of_bears_with_goofy_temperaments
    "SELECT COUNT(*) FROM bears WHERE temperament = 'goofy'"
 end
  def selects_bear_that_killed_Tim
